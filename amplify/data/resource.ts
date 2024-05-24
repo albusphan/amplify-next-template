5b305507191557b886a57888583dea271ee368c5
Minh Phan <pnminh1710@gmail.com>
2024-05-24 14:30:46 +0700

    feat: add product data schema

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,9 +7,24 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
+  Customer: a.model({
+    name: a.string(),
+    id: a.string(),
+    phoneNumber: a.string(),
+    stringTickets: a.hasMany('StringTicket', 'customerId'),
+  }).authorization((allow) => [allow.publicApiKey()]),
+  Product: a.model({
+    id: a.id().required(),
+    name: a.string().required(),
+    price: a.integer().required(),
+    brand: a.string().required(),
+  }),
+  StringTicket: a.
+    model({
+      productId: a.id(),
+      product: a.hasOne('Product', 'productId'),
+      customerId: a.id(),
+      customer: a.belongsTo('Customer', 'customerId'),
     })
     .authorization((allow) => [allow.publicApiKey()]),
 });
@@ -31,7 +46,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
@@ -7,46 +10,90 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+
 
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
-export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+const productFormSchema = z.object({
+  name: z.string(),
+  price: z.number(),
+  brand: z.string(),
+})
 
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
+type ProductForm = z.infer<typeof productFormSchema>
+
+export default function App() {
+  const [products, setProducts] = useState<Array<Schema["Product"]["type"]>>([]);
+
+  const form = useForm<ProductForm>({
+    defaultValues: {
+      name: "",
+      price: 0,
+      brand: "",
+    }
+  })
+
+  function listProducts() {
+    client.models.Product.observeQuery().subscribe({
+      next: (data) => setProducts([...data.items]),
     });
+  }
+
+
+  function deleteProduct(id: string) {
+    client.models.Product.delete({ id })
   }
 
   useEffect(() => {
-    listTodos();
+    listProducts();
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
+
+  function createProduct() {
+    client.models.Product.create({
+      name: "BG-65",
+      price: 120000,
+      brand: "Yonex",
     });
   }
 
+  function onSubmit(data: ProductForm) {
+    client.models.Product.create(data)
+  }
+
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
+
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>My product</h1>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <input {...form.register("name")} />
+            <input {...form.register("price")} />
+            <input {...form.register("brand")} />
+            <button type="submit">Create Product</button>
+          </form>
+          <ul>
+            {products.map((product) => (
+              <li
+                onClick={() => deleteProduct(product.id)}
+                key={product.id}>{product.name} - {product.brand}</li>
+            ))}
+          </ul>
+          <div>
+            🥳 App successfully hosted. Try creating a new product
+            <br />
+            <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+              Review next steps of this tutorial.
+            </a>
+          </div>
+          <button onClick={signOut}>Sign out</button>
+        </main>)}
+    </Authenticator>
   );
 }
